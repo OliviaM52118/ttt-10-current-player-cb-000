014cfaef707548ad05c67f6348a9ef085210fff6
Olivia Macedo <liom62313@gmail.com>
2018-08-24 03:03:44 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,15 @@ def turn_count(board)
 end
 
 def current_player(board)
-
-
-
+  temp1 = 0
+  temp2 = turn_count(board)
+  temp1 = temp2 % 2
+  answer = ""
+  if (temp1 == 0)
+    answer = "X"
+    return answer
+  else
+    answer = "O"
+    return answer
+  end
 end
